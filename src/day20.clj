8de064d005a867cff4bf6435949c2238243708f4
Daniel Yankowsky <bytecrafter@gmail.com>
2017-09-04 22:18:04 -0400

    day20 part2

--- src/day20.clj
+++ src/day20.clj
@@ -14,11 +14,23 @@
         ext (if (zero? (mod n lim)) [(int lim)] [])]
     (sort (concat ext factors))))
 
-(defn compute-house-presents [house-number]
+(defn compute-house-presents-part1 [house-number]
   {:house-number house-number
    :present-count (* 10 (apply + (factorize house-number)))})
 
+(defn compute-house-presents-part2 [house-number]
+  (let [factors (factorize house-number)
+        llim (Math/ceil (/ house-number 50))
+        ffactors (remove #(< % llim) factors)]
+    {:house-number house-number
+     :present-count (* 11 (apply + ffactors))}))
+
 (defn part1 []
-  (let [present-counts (pmap compute-house-presents (rest (range)))
+  (let [present-counts (pmap compute-house-presents-part1 (rest (range)))
+        houses-with-enough-presents (filter #(>= (:present-count %) input) present-counts)]
+    (:house-number (first houses-with-enough-presents))))
+
+(defn part2 []
+  (let [present-counts (pmap compute-house-presents-part2 (rest (range)))
         houses-with-enough-presents (filter #(>= (:present-count %) input) present-counts)]
     (:house-number (first houses-with-enough-presents))))
