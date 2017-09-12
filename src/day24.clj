28e58cc60b47fe8e1ad90166c53ae94142e62c44
Daniel Yankowsky <bytecrafter@gmail.com>
2017-09-12 01:59:49 -0400

    day24 part1 part2

---
+++ src/day24.clj
@@ -0,0 +1,31 @@
+(ns day24
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [advent-util :as advent]
+            [clojure.string :as str]))
+
+(def input-string (slurp (io/resource "day24_input.txt")))
+
+(def input (map advent-util/parse-int (str/split-lines input-string)))
+
+(defn subsets [coll]
+  (apply concat
+         (for [n (range (inc (count coll)))]
+           (advent-util/combinations coll n))))
+
+(defn solve [weights num-groups]
+  (let [total-weight (reduce + 0 weights)
+        target-weight (quot total-weight num-groups)]
+    (assert (zero? (mod total-weight target-weight)))
+    (let [choices (subsets (sort > weights))
+          interesting-choices (filter #(= (reduce + 0 %) target-weight) choices)
+          shortest-choices (first (partition-by count interesting-choices))
+          entanglements (map #(apply * %) shortest-choices)]
+      (apply min entanglements))))
+
+(defn part1 []
+  (solve input 3))
+
+(defn part2 []
+  (solve input 4))
+
